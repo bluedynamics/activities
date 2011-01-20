5ca3e669d5e31d30b1109c894073411a4268bbac
Johannes Raggam <raggam-nl@adm.at>
2011-01-20 20:21:26 +0100

    cleanup

--- src/activities/tests/testrunner.py
+++ src/activities/tests/testrunner.py
@@ -1,12 +1,3 @@
-# -*- coding: utf-8 -*-
-#
-# Copyright 2009: Johannes Raggam, BlueDynamics Alliance
-#                 http://bluedynamics.com
-# GNU Lesser General Public License Version 2 or later
-
-__author__ = """Johannes Raggam <johannes@raggam.co.at>"""
-__docformat__ = 'plaintext'
-
 import unittest
 import doctest
 from interlude import interact
